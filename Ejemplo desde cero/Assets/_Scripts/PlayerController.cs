121f39986000a11368d509c07e854bafb879b98c
DESKTOP-1P1H21M\carlo <antoniocarlos.ordonez@gmail.com>
2018-01-09 10:25:47 +0100

    Ahora la bola puede rodar. Se han añadido luces.

---
+++ Ejemplo desde cero/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float Speed = 5f;
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+         rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // transform.Translate(Input.GetAxis("Horizontal") * Speed, 0, Input.GetAxis("Vertical") * Speed);
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical") * Speed);
+    }
+}
